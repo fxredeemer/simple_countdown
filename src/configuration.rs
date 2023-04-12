24b26da899b7cde0803a81185e7b31c0d1b27826
Flurin Feuerstein <ffeuerstein@hamilton.ch>
2023-04-12 14:17:18 +0200

    support d.m.yy date format

--- src/configuration.rs
+++ src/configuration.rs
@@ -17,8 +17,8 @@ fn parse_exclusions(
         format!("Invalid exclusion date format: Must be yyyy-mm-dd/yyyy-mm-dd in `{exclusions}`")
     })?;
 
-    let begin = exclusions[..pos].parse()?;
-    let end = exclusions[pos + 1..].parse()?;
+    let begin = ensure_iso_date_string(&exclusions[..pos]).parse()?;
+    let end = ensure_iso_date_string(&exclusions[pos + 1..]).parse()?;
 
     if end < begin {
         Err(format!(
@@ -30,9 +30,9 @@ fn parse_exclusions(
 }
 
 fn parse_target_date(
-    exclusions: &str,
+    target_date: &str,
 ) -> Result<NaiveDate, Box<dyn Error + Send + Sync + 'static>> {
-    let end_date = exclusions.parse::<NaiveDate>()?;
+    let end_date = ensure_iso_date_string(target_date).parse::<NaiveDate>()?;
     let today = Local::now().date_naive();
 
     if today > end_date {
@@ -40,4 +40,15 @@ fn parse_target_date(
     }
 
     Ok(end_date)
+}
+
+fn ensure_iso_date_string(date: &str) -> String {
+    let mut year_month_day_collection: Vec<&str> = date.split('.').collect();
+
+    if year_month_day_collection.len() != 3 {
+        return date.to_owned();
+    }
+
+    year_month_day_collection.reverse();
+    year_month_day_collection.join("-")
 }
